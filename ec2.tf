e1a3d1a13b494ec96c5e89b9d6b20bb5e48f9502
pasima-m <111281963+pasima-m@users.noreply.github.com>
2022-12-07 17:29:09 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-      region = "eu-west-2"
+  region = "eu-west-2"
 }
-
 resource "aws_instance" "web" {
   ami           = "ami-03542b5588cd0e6b3"
-  security_groups = ["jkSG"]    
+  security_groups = ["jkSG"]
   instance_type = "t2.micro"
   key_name      =  "delete"
+
   tags = {
-    Name = "tomcat"
-    Enviroment = "dev    
+    Name = "Tomcat"
+    Enviroment = dev
   }
 }
 
