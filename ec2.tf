e582bb39f466ccd3f4b6571df600aead0e5c1c73
pasima-m <111281963+pasima-m@users.noreply.github.com>
2022-12-07 17:52:11 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 provider "aws" {
       region = "eu-west-2"
 }
-resource "aws_instance" "app" {
+
+resource "aws_instance" "web" {
     ami           = "ami-03542b5588cd0e6b3"
     instance_type = "t2.micro"
     security_groups = ["jkSG"]
